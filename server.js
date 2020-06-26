dde23cf86755d1f43bf40d5395749e9b7fa14b39
sumand99 <sumand99@192.168.1.6>
2020-06-26 12:15:31 -0700

    update the port to env.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 app.set('view engine','ejs');
 
@@ -11,5 +11,6 @@ app.get('/',(req,res)=>{
 
 
 app.listen(port, function() {
+  console.log("process.env.PORT = " + port);
   console.log("Server is running on Port: " + port);
 });
